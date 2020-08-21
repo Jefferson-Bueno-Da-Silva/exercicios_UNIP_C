42c2d85d41d558127d88a72085867f6bfe09f32f
Jefferson-Bueno-Da-Silva <62211295+Jefferson-Bueno-Da-Silva@users.noreply.github.com>
2020-08-21 20:39:33 -0300

    exercício 02
    2 - Com a função prinf gere a seguinte saída no console (sim com a leta T):
                              TTTTTTTTTTTTTTT
                              TTTTTTTTTTTTTTT
                                          TTT
                                          TTT
                                          TTT
                                          TTT

---
+++ exercicio_2.c
@@ -0,0 +1,21 @@
+/*
+Com a função prinf gere a seguinte saída no console (sim com a leta T):
+                    TTTTTTTTTTTTT
+                    TTTTTTTTTTTTT
+                         TTT
+                         TTT
+                         TTT
+                         TTT
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("Tzao com Tzinhos:\n");
+    printf("TTTTTTTTTTTTTTT\nTTTTTTTTTTTTTTT\n\40\40\40\40\40\40TTT\n\40\40\40\40\40\40TTT\n\40\40\40\40\40\40TTT\n\40\40\40\40\40\40TTT\n\40\40\40\40\40\40TTT\n");
+
+    system("pause");
+    return 0;
+}
